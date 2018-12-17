8611a42d0a6b067aa794f2826ba836487f0a7f92
Rob Little <rlittletht@users.noreply.github.com>
2018-12-16 23:59:51 -0800

    Hooked up authentication to the webapi. By adding [Authorize], this requires authorization in order for the api to be invoked.
    By adding Microsoft.Owin.Host.SystemWeb we can configure the app to use OauthBearer authentication. (This also involves implementing a rudimentary token cache, stolen from another sample).
    
    Added lots of nuget packages to get this to work with MSAL (Microsoft.IdentityModel.*):
    Microsoft.IdentityModel.JsonWebTokens version="5.3.0"
    Microsoft.IdentityModel.Logging version="5.3.0"
    Microsoft.IdentityModel.Protocols version="5.3.0"
    Microsoft.IdentityModel.Protocols.OpenIdConnect version="5.3.0"
    Microsoft.IdentityModel.Tokens version="5.3.0"
    Microsoft.Owin version="4.0.0"
    Microsoft.Owin.Host.SystemWeb version="4.0.0"
    Microsoft.Owin.Security version="4.0.0"
    Microsoft.Owin.Security.Jwt version="4.0.0"
    Microsoft.Owin.Security.OAuth version="4.0.0"
    Owin version="1.0"
    System.IdentityModel.Tokens.Jwt version="5.3.0"
    
    ** At this point, calling the WebApi from the WebApp is going to fail. Even though the WebApi is setup to accept AccessTokens from the WebApp client, the WebApp is not yet passing any authentication to the WebApi.

---
+++ WebApi/Startup.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(WebApi.Startup))]
+
+namespace WebApi
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // Invoke the ConfigureAuth method, which will set up
+            // the OWIN authentication pipeline using OAuth 2.0
+
+            ConfigureAuth(app);
+        }
+
+        public void ConfigureServices()
+        {
+        }
+    }
+}
