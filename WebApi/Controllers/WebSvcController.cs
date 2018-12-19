d21ea4700222e3e74d576e45ca55ad3daa244884
Rob Little <rlittletht@users.noreply.github.com>
2018-12-18 16:03:59 -0800

    Include claims information in the webapi response.

--- WebApi/Controllers/WebSvcController.cs
+++ WebApi/Controllers/WebSvcController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Security.Claims;
 using System.Web.Http;
 
 namespace WebApi.Controllers
@@ -10,9 +11,31 @@ namespace WebApi.Controllers
     [Authorize]
     public class WebSvcController : ApiController
     {
+        string GetCurrentClaimsDescription()
+        {
+            string sEmail = ClaimsPrincipal.Current.FindFirst("preferred_username")?.Value;
+            string sScopeClaim = ClaimsPrincipal.Current.FindFirst("http://schemas.microsoft.com/identity/claims/scope")?.Value;
+
+            return $"Provided Claims: Email({sEmail}), Scope({sScopeClaim})";
+        }
+
+        /*----------------------------------------------------------------------------
+        	%%Function: GetTestResult
+        	%%Qualified: WebApi.Controllers.WebSvcController.GetTestResult
+
+            The goal here is to prepend the given id with information about the user
+            (both acquired by the token used to call this WebApi, as well as by
+            calling the MS Graph to get profile information about the user that
+            called this WebApi)
+        	
+        ----------------------------------------------------------------------------*/
         public IHttpActionResult GetTestResult(string id)
         {
-            return Ok(id);
+            string sClaimsDescription = GetCurrentClaimsDescription();
+
+            string sReturn = $"Context: [{sClaimsDescription}]; Return: {id}";
+
+            return Ok(sReturn);
         }
     }
 }
